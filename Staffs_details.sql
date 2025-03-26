fe222b4fc06ced94a8a7e5ed3ba68703d9756206
brian <otienolubalo@gmail.com>
2025-03-26 23:15:27 +0300

    keyed in data into the staff table

---
+++ Staffs_details.sql
@@ -0,0 +1,12 @@
+INSERT INTO Staffs_Data(Department_name,Employee_id_No,Designation)
+VALUES
+("IT", "2850","HOD"),
+("Finance","2916","D.HOD"),
+("Public ralation","2800","Supervisor"),
+("Electrical","2803","Supervisor"),
+("Social work","2706","Clerk"),
+("Education","2800","Principal Officer"),
+("Medical","3109","Nurse");
+
+SELECT * FROM Staffs_Data;
+
