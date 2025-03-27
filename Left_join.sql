474905e6333aae8698bb58eebd7652597cc8b5b0
brian <otienolubalo@gmail.com>
2025-03-27 22:28:27 +0300

    left join,retrieve all rows from the left table and the matched rows from the right table. Includes all records from the left table (Table1), and the matched records from the right table (Table2).

---
+++ Left_join.sql
@@ -0,0 +1,12 @@
+SELECT
+   s.Employee_id_No,
+    s.Department_name,
+    s.Designation,
+    e.Full_Name,
+    e.Email,
+    e.Birth_year,
+    e.Phone_Number
+FROM Staffs_Data s
+LEFT JOIN Employee_Personal_Info e
+ON s.Employee_id_No = e.Employee_id_No
+ORDER BY Employee_id_No DESC
