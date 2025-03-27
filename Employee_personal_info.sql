d31515f6b13b485292c39d2969f8a42a7589c6d8
brian <otienolubalo@gmail.com>
2025-03-27 16:27:38 +0300

    inserted data into the table using sql

---
+++ Employee_personal_info.sql
@@ -0,0 +1,17 @@
+INSERT INTO Employee_Personal_Info (
+    Employee_id_No, 
+    Full_Name, 
+    Email, 
+    Birth_year,
+    Phone_Number
+)
+VALUES
+(5993, "Otieno Kimani", "otienokimani@gamil.com", "2002", "0788968993")
+(2850, "Allan Kimani", "allankimani@gmail.com", "1986", "0788201398"),
+(2916, "Kimani Otieno", "kimaniotieno@gmail.com", "1980", "0714523689"),
+(2001, "Vincent Wanjala", "vincentwanjala@gmail.com", "1961", "075689412"),
+(2803, "Susan Neema", "susaneema@gmail.com", "1980", "0712345678"),
+(3109, "Cynthia Omondi", "omondicynt@gmail.com", "2001", "078963524"),
+(2808, "Millicent Yetu", "millicentyetu@gmail.com", "2000", "077856932");
+
+SELECT * FROM Employee_Personal_Info;
